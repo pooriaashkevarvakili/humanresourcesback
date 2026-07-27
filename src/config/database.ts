c5309538e69be980c1212d97dee74b41cfe4100c
pooriaashkevarvakili <pooriavakili09@gmail.com>
2026-07-27 19:29:03 +0330

    fixed

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ import { User } from "../entities/User";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-     //  url:'postgresql://project_dashboard_gk1z_user:61hfaDfadNapDaIL9EdMP9ii1i9nMf30@dpg-d919j8u7r5hc73cjfu60-a/project_dashboard_gk1z',
+     url:'postgresql://project_dashboard_gk1z_user:61hfaDfadNapDaIL9EdMP9ii1i9nMf30@dpg-d919j8u7r5hc73cjfu60-a/project_dashboard_gk1z',
 
   host: "localhost",
   port: 5432,
