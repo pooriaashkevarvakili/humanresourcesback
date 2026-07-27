a6ddcf09154540751fda799695ff51f0b9e1eacf
pooriaashkevarvakili <pooriavakili09@gmail.com>
2026-07-27 19:20:09 +0330

    fixed database

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,16 +4,17 @@ import { User } from "../entities/User";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-    url: "postgresql://project_dashboard_gk1z_user:61hfaDfadNapDaIL9EdMP9ii1i9nMf30@dpg-d919j8u7r5hc73cjfu60-a.oregon-postgres.render.com:5432/project_dashboard_gk1z",
+       url:'postgresql://project_dashboard_gk1z_user:61hfaDfadNapDaIL9EdMP9ii1i9nMf30@dpg-d919j8u7r5hc73cjfu60-a/project_dashboard_gk1z',
 
+  host: "localhost",
   port: 5432,
-  username: "project_dashboard_gk1z_user",
-  password: "PASSWORD",
-  database: "project_dashboard_gk1z",
-  ssl: {
-    rejectUnauthorized: false,
-  },
+
+  username: "postgres",
+  password: "13711373n",
+  database: "humanresources",
+
   entities: [User],
+
   synchronize: true,
   logging: true,
 });
