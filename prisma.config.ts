c6ff6560e7174557399f686e1abe988fd07ea8d8
pooriaashkevarvakili <pooriavakili09@gmail.com>
2026-07-27 17:20:06 +0330

    fixed prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,9 @@
-import "dotenv/config";
-
-
-import { defineConfig, env } from "prisma/config";
-
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
 
   datasource: {
-    url: env("DATABASE_URL"),
+    url: "postgresql://project_dashboard_gk1z_user:PASSWORD@HOST:5432/project_dashboard_gk1z",
   },
 });
