7957de183b10e794ff413bef66eaea771182112a
pooriaashkevarvakili <pooriavakili09@gmail.com>
2026-07-27 17:26:55 +0330

    fixed prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-
   datasource: {
-    url: "postgresql://project_dashboard_gk1z_user:PASSWORD@HOST:5432/project_dashboard_gk1z",
+    url: "postgresql://project_dashboard_gk1z_user:61hfaDfadNapDaIL9EdMP9ii1i9nMf30@dpg-d919j8u7r5hc73cjfu60-a.oregon-postgres.render.com:5432/project_dashboard_gk1z",
   },
 });
