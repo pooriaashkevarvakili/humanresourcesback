f3966a9db7f06b2041337acb06760c2e8af3ec08
pooriaashkevarvakili <pooriavakili09@gmail.com>
2026-07-28 10:32:45 +0330

    fixed server

--- dist/app.js
+++ dist/app.js
@@ -20,7 +20,9 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: true,
+    origin: [
+        "http://localhost:5173"
+    ],
     credentials: true,
 }));
 app.use((0, helmet_1.default)({
--- src/app.ts
+++ src/app.ts
@@ -21,10 +21,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin: true,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173"
+    ],
+    credentials: true,
+  })
+);
 
 app.use(helmet({
   contentSecurityPolicy: false,
