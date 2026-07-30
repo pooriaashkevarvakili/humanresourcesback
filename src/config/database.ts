f204e92eb284f7c6fa1622862b32382cbef5d413
pooriaashkevarvakili <pooriavakili09@gmail.com>
2026-07-30 15:33:44 +0330

    fixed database

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,23 +3,19 @@ import { DataSource } from "typeorm";
 import { User } from "../entities/User";
 
 export const AppDataSource = new DataSource({
-
   type: "postgres",
 
-  url: "postgresql://project_dashboard_gk1z_user:61hfaDfadNapDaIL9EdMP9ii1i9nMf30@dpg-d919j8u7r5hc73cjfu60-a/project_dashboard_gk1z",
-//username: "postgres",
-//password: "13711373n",
-database: "humanresources",
+  url: "postgresql://project_dashboard_gk1z_user:xxxxx@dpg-d919j8u7r5hc73cjfu60-a.oregon-postgres.render.com:5432/project_dashboard_gk1z",
+  //username: "postgres",
+  //password: "13711373n",
+  database: "humanresources",
   ssl: {
     rejectUnauthorized: false,
   },
 
-  entities: [
-    User
-  ],
-
+  entities: [User],
 
   synchronize: true,
 
- // logging: true,
+  // logging: true,
 });
